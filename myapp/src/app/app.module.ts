316ec06b04693a67d1d088575d42c1f37b073152
Morgan <morgan.espindola@gmail.com>
2021-06-19 11:47:28 -0300

    Seletores

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MyFirstComponent } from './myfirst.component';
+import { Test1Component } from './tests/test1/test1.component';
+import { Test2Component } from './tests/test2/test2.component';
+import { Test3Component } from './tests/test3/test3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFirstComponent
+    MyFirstComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component
   ],
   imports: [
     BrowserModule
