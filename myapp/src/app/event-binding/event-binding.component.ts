5048055a5acdab97ed24bb6e3b5364b8fce5235a
Morgan <morgan.espindola@gmail.com>
2021-06-20 13:49:29 -0300

    Exercício - Event Binding
    A página deve mostrar outro spinner que tem mode determinado, que aumenta conforme o número de cliques no botão.
    
    Extra: sempre que circulo chegar ao máximo, deve ser informado ao usuário que o próximo clique vai resetar o circulo e o valor do spinner deve voltar a 1

--- myapp/src/app/event-binding/event-binding.component.ts
+++ myapp/src/app/event-binding/event-binding.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import {ProgressSpinnerMode} from '@angular/material/progress-spinner';
+import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'myapp-event-binding',
   templateUrl: './event-binding.component.html',
   styleUrls: ['./event-binding.component.css']
 })
+
+
 export class EventBindingComponent implements OnInit {
 
+  FULL_CIRLE = 100
   buttonName = "My button"
   i = 0
+  spinnerValue = 0
   spinnerMode: ProgressSpinnerMode = 'determinate';
   btnEnable = true
 
@@ -18,16 +22,21 @@ export class EventBindingComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  save(){
+  save() {
     console.log('Click')
   }
 
-  increment(){
-    this.i++;
-    this.buttonName = "It was clicked "+ this.i + " times"
+  increment() {
+    this.i++
+    let rest = this.i % this.FULL_CIRLE
+    this.spinnerValue = rest === 0 ? this.FULL_CIRLE : rest
+    this.buttonName = "You clicked me " + this.i + " times"
+    if (rest === 0) {
+      this.buttonName += " - You are going to reset the spinner value!!!"
+    }
   }
 
-  disable(){
+  disable() {
     this.btnEnable = false
     this.spinnerMode = 'indeterminate';
     setTimeout(() => {
