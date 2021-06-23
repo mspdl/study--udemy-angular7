d8bd8bdc15e976b3eb11d902fe0fbac38b659436
Morgan <morgan.espindola@gmail.com>
2021-06-23 09:58:11 -0300

    ngClass

--- directives-app/src/app/app.module.ts
+++ directives-app/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './ng-style/ng-style.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgStyleComponent
+    NgStyleComponent,
+    NgClassComponent
   ],
   imports: [
     BrowserModule,
