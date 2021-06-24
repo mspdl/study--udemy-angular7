7b6ae53128174b1ebb9212aa11a78ca332104b78
Morgan <morgan.espindola@gmail.com>
2021-06-23 23:20:50 -0300

    Input Binding

--- communication-app/src/app/app.module.ts
+++ communication-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputBindingComponent } from './input-binding/input-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputBindingComponent
   ],
   imports: [
     BrowserModule,
