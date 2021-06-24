8150f09f013299e46ba5aec50c22efd39c3c8fa0
Morgan <morgan.espindola@gmail.com>
2021-06-24 10:29:51 -0300

    Gerando eventos com EventEmmiter

--- communication-app/src/app/app.module.ts
+++ communication-app/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { ChildItemComponent } from './event/child-item/child-item.component';
+import { EventComponent } from './event/event.component';
 import { ClientComponent } from './input-binding/client/client.component';
 import { InputBindingComponent } from './input-binding/input-binding.component';
 
@@ -11,12 +15,16 @@ import { InputBindingComponent } from './input-binding/input-binding.component';
   declarations: [
     AppComponent,
     InputBindingComponent,
-    ClientComponent
+    ClientComponent,
+    EventComponent,
+    ChildItemComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
