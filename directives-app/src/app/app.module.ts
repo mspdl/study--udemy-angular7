f899e4a8c5d5c5e16af4b8cb7e28a81ea2845c13
Morgan <morgan.espindola@gmail.com>
2021-06-24 22:12:37 -0300

    ngfor - Parte 1

--- directives-app/src/app/app.module.ts
+++ directives-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NgClassComponent } from './ng-class/ng-class.component';
+import { NgForComponent } from './ng-for/ng-for.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
 import { SubNgIfComponent } from './ng-if/sub-ng-if/sub-ng-if.component';
 import { NgStyleComponent } from './ng-style/ng-style.component';
@@ -21,7 +23,8 @@ import { NgStyleComponent } from './ng-style/ng-style.component';
     NgStyleComponent,
     NgClassComponent,
     NgIfComponent,
-    SubNgIfComponent
+    SubNgIfComponent,
+    NgForComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +35,8 @@ import { NgStyleComponent } from './ng-style/ng-style.component';
     MatFormFieldModule,
     MatSelectModule,
     MatCheckboxModule,
-    MatInputModule
+    MatInputModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
