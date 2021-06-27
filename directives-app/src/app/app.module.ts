991cf07a020cbd434c7ccddbd4766e35b0969f45
Morgan <morgan.espindola@gmail.com>
2021-06-27 10:35:54 -0300

    ngContent

--- directives-app/src/app/app.module.ts
+++ directives-app/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { NgStyleComponent } from './ng-style/ng-style.component';
 import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 import { NgTemplateComponent } from './ng-template/ng-template.component';
 import { NgContainerComponent } from './ng-container/ng-container.component';
+import { NgContentComponent } from './ng-content/ng-content.component';
 
 
 @NgModule({
@@ -35,7 +36,8 @@ import { NgContainerComponent } from './ng-container/ng-container.component';
     NgForFormComponent,
     NgSwitchComponent,
     NgTemplateComponent,
-    NgContainerComponent
+    NgContainerComponent,
+    NgContentComponent
   ],
   imports: [
     BrowserModule,
