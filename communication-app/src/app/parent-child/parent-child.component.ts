2ef0b334bb0c5002de06d54297cad486587bbe0a
Morgan <morgan.espindola@gmail.com>
2021-06-30 20:50:58 -0300

    ViewChild

--- communication-app/src/app/parent-child/parent-child.component.ts
+++ communication-app/src/app/parent-child/parent-child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-parent-child',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentChildComponent implements OnInit {
 
+  @ViewChild("timer2")
+  myTimer!: TimerComponent;
+
+  @ViewChild("myP")
+  myP!: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){
+    console.log(this.myP);
+  }
+
+  start(){
+    this.myTimer.start();
+  }
+
+  stop(){
+    this.myTimer.stop();
+  }
+
+  clear(){
+    this.myTimer.clear();
   }
 
 }
