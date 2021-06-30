bba9c11fcf4b46a917ab1d0d375f8310b0aa99fa
Morgan <morgan.espindola@gmail.com>
2021-06-29 22:45:27 -0300

    Variáveis Template

--- communication-app/src/app/app.module.ts
+++ communication-app/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ClientsComponent } from './clients/clients.component';
+import { ItemClientComponent } from './clients/item-client/item-client.component';
 import { ChildItemComponent } from './event/child-item/child-item.component';
 import { EventComponent } from './event/event.component';
 import { ClientComponent } from './input-binding/client/client.component';
 import { InputBindingComponent } from './input-binding/input-binding.component';
-import { FormsModule } from '@angular/forms';
-import { ItemClientComponent } from './clients/item-client/item-client.component';
+import { ParentChildComponent } from './parent-child/parent-child.component';
+import { TimerComponent } from './parent-child/timer/timer.component';
 
 
 @NgModule({
@@ -24,7 +27,9 @@ import { ItemClientComponent } from './clients/item-client/item-client.component
     EventComponent,
     ChildItemComponent,
     ClientsComponent,
-    ItemClientComponent
+    ItemClientComponent,
+    ParentChildComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +39,8 @@ import { ItemClientComponent } from './clients/item-client/item-client.component
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
