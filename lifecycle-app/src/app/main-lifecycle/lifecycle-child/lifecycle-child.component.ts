17f20f92e4bb5c0264d1db59d74b2fada81aaa61
Morgan <morgan.espindola@gmail.com>
2021-07-02 08:46:15 -0300

    Lifecycle Hooks

--- lifecycle-app/src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
+++ lifecycle-app/src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
@@ -1,19 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-lifecycle-child',
   templateUrl: './lifecycle-child.component.html',
   styleUrls: ['./lifecycle-child.component.css']
 })
-export class LifecycleChildComponent implements OnInit {
+export class LifecycleChildComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit, AfterContentInit {
 
   @Input() name!: string;
   @Input() age!: number | undefined;
   @Input() food!: string;
 
-  constructor() { }
+  constructor() { // First Call
+    console.log(this.name + " - constructor")
+   }
 
-  ngOnInit(): void {
+   ngOnChanges() { // Second Call
+    console.log(this.name + " - ngOnChanges")
+  }
+
+  ngOnInit() { // Third Call
+    console.log(this.name + " - ngOnInit")
+  }
+
+  ngAfterContentInit(){ // Fourth Call
+    console.log(this.name + " - ngAfterContentInit")
+  }
+
+  ngAfterViewInit(){ // Fifth Call
+    console.log(this.name + " - ngAfterViewInit")
+  }
+
+  ngOnDestroy() {
+    console.log(this.name + " - ngOnDestroy")
   }
 
 }
